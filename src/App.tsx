6c0441aa14fed4fb56b8bc9814f8bab4b6fba17b
myaimcp <srinisankar@hotmail.com>
2025-05-30 15:16:03 -0400

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+// src/App.tsx
+import WWDCMomentumDashboard from './WWDCMomentumDashboard';
+
+function App() {
+  return <WWDCMomentumDashboard />;
+}
+
+export default App;   // ← this is the “export” part
